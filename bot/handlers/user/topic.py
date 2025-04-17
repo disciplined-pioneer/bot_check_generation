66085d182fd10e342c05cab7adb441181f8cdf40
Virtu <maksimsarsov777@gmail.com>
2025-04-17 23:24:24 +0300

    update

--- bot/handlers/user/topic.py
+++ bot/handlers/user/topic.py
@@ -11,53 +11,52 @@ router = Router()
 # Отправка сообщений в ЛС из топика
 @router.message(F.chat.id == settings.bot.GROUP_ID)
 async def handle_message_in_topic(message: Message):
-    
-    user = message.from_user
+
+    print(message.from_user)
     topic_id = message.message_thread_id
     tg_id = await UserTopics.get_topic_id_by_tg_id(topic_id)
-    if topic_id and not user.is_bot:
-        try:
-            # Текст
-            if message.text:
-                await bot.send_message(chat_id=tg_id, text=message.text)
+    try:
+        # Текст
+        if message.text:
+            await bot.send_message(chat_id=tg_id, text=message.text)
 
-            # Фото
-            elif message.photo:
-                await bot.send_photo(
-                    chat_id=tg_id,
-                    photo=message.photo[-1].file_id,  # Самое большое по размеру
-                    caption=message.caption or ""
-                )
+        # Фото
+        elif message.photo:
+            await bot.send_photo(
+                chat_id=tg_id,
+                photo=message.photo[-1].file_id,  # Самое большое по размеру
+                caption=message.caption or ""
+            )
 
-            # Документ
-            elif message.document:
-                await bot.send_document(
-                    chat_id=tg_id,
-                    document=message.document.file_id,
-                    caption=message.caption or ""
-                )
+        # Документ
+        elif message.document:
+            await bot.send_document(
+                chat_id=tg_id,
+                document=message.document.file_id,
+                caption=message.caption or ""
+            )
 
-            # Видео
-            elif message.video:
-                await bot.send_video(
-                    chat_id=tg_id,
-                    video=message.video.file_id,
-                    caption=message.caption or ""
-                )
+        # Видео
+        elif message.video:
+            await bot.send_video(
+                chat_id=tg_id,
+                video=message.video.file_id,
+                caption=message.caption or ""
+            )
 
-            # Голосовое сообщение
-            elif message.voice:
-                await bot.send_voice(
-                    chat_id=tg_id,
-                    voice=message.voice.file_id,
-                    caption=message.caption or ""
-                )
+        # Голосовое сообщение
+        elif message.voice:
+            await bot.send_voice(
+                chat_id=tg_id,
+                voice=message.voice.file_id,
+                caption=message.caption or ""
+            )
 
-            else:
-                print("⚠️ Тип сообщения не поддержан:", message)
+        else:
+            print("⚠️ Тип сообщения не поддержан:", message)
 
-        except Exception as e:
-            print(f"\n❌ Ошибка при отправке сообщения пользователю: {e}\n")
+    except Exception as e:
+        print(f"\n❌ Ошибка при отправке сообщения пользователю: {e}\n")
 
 
 # Обработка сообщений от пользователя в БОТЕ
